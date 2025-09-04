9530ac074c8cc6e6c2d26487670946ea3abab6b9
Peter Mollmann <thorhaugemoellmann@gmail.com>
2025-09-04 09:33:58 +0200

    added latin hypercube sampling for determining the material properties

---
+++ material_parameter_generator.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from scipy.stats import qmc
+
+# import numpy as np
+
+
+param_ranges = {
+    "E": (180e3, 220e3),  # Young's modulus [MPa]
+    "nu": (0.25, 0.35),  # Poisson's ratio [-]
+    "A": (400, 700),  # JC hardening A (yield strength) [MPa]
+    "B": (800, 1200),  # JC hardening B [MPa]
+    "n": (0.15, 0.35),  # JC hardening exponent [-]
+    "D1": (0.02, 0.10),  # JC damage parameter [-]
+    "D2": (0.05, 0.15),  # JC damage parameter [-]
+    "D3": (-0.7, -0.3),  # JC damage parameter [-]
+    "uts": (1000, 1300),  # Ultimate tensile strength [MPa]
+}
+
+n_samples = 10
+output_file = "material_parameters.csv"
+
+
+dim = len(param_ranges)
+sampler = qmc.LatinHypercube(d=dim, seed=42)
+sample = sampler.random(n=n_samples)
+
+l_bounds = [low for low, high in param_ranges.values()]
+u_bounds = [high for low, high in param_ranges.values()]
+scaled_sample = qmc.scale(sample, l_bounds, u_bounds)
+
+
+df = pd.DataFrame(scaled_sample, columns=param_ranges.keys())
+df.insert(0, "id", [f"{i:04d}" for i in range(1, n_samples + 1)])
+
+df = df.round(
+    {
+        "E": 0,
+        "nu": 3,
+        "A": 0,
+        "B": 0,
+        "n": 3,
+        "D1": 3,
+        "D2": 3,
+        "D3": 3,
+        "uts": 0,
+    }
+)
+df.to_csv(output_file, index=False)
