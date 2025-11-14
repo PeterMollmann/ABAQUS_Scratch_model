b7d53b7d233f6edbc3cc370dafd45d15f1310a5e
Peter Mollmann <thorhaugemoellmann@gmail.com>
2025-11-14 13:55:47 +0100

    Refactor MassScaleConvergence and MeshConvergence scripts to improve material parameter handling and job submission process; include wear functionality and update output file management.

--- MassScaleConvergence.py
+++ MassScaleConvergence.py
@@ -4,7 +4,11 @@ from ProgressiveLoadScratch.PostProcessing import *
 from ProgressiveLoadScratch.ProgressiveLoadScratchTest import ScratchModelSetup
 from ProgressiveLoadScratch.SubstrateMaterial import SubstrateMaterialAssignment
 import os
-from material_parameters import parameters
+
+# from material_parameters import parameters
+import shutil
+from cleanup import cleanupAbaqusJunk
+import ProgressiveLoadScratch.Constants as C
 
 ### ---------------- ###
 # SETTINGS
@@ -12,16 +16,14 @@ from material_parameters import parameters
 jobName = "MassScaleConvergence"
 
 # Array of shape (#mesh combinations, 3). Rows are mesh combinations, coloums are [meshSizeX, meshSizeY, meshSizeZ],
-massScales = [1e1, 1e2, 1e3, 1e4, 5e4, 1e5]
+# massScales = [1e5, 5e4, 1e4, 1e3, 1e2, 1e1]
+# massScales = [1e3, 1e2, 1e1]
+massScales = [4e5]
 
 
 meshSize = [0.040, 0.030, 0.020, 0.010, 0.005]
-meshSizeIdx = 0
+meshSizeIdx = 3
 
-
-# Parallelisation
-num_cpus = 6
-num_domains = num_cpus
 
 # Change abaqus working directory
 rundir = os.path.join("runs", "MassScaleConvergence")
@@ -30,54 +32,69 @@ if not os.path.exists(rundir):
 os.chdir(rundir)
 
 
-depth = -50e-3
-arg = parameters[0]
-run_id = arg["id"]
-rho = float(arg["rho"])
-E = float(arg["E"])
-nu = float(arg["nu"])
-A = float(arg["A"])
-B = float(arg["B"])
-n = float(arg["n"])
-D1 = float(arg["D1"])
-D2 = float(arg["D2"])
-D3 = float(arg["D3"])
-uts = float(arg["uts"])
-kc = float(arg["kc"])
-mu = float(arg["mu"])
+rho = 8.0e-09
+E = 200000.0
+nu = 0.3
+A = 700.0
+B = 700.0
+n = 0.5
+D1 = 1.0
+D2 = 0.5
+D3 = -0.25
+uts = 1000.0
+kc = 2000
+mu = 0.1
+kappa = 1e-4
 
+# make to dict
+material_params = {
+    "rho": rho,
+    "E": E,
+    "nu": nu,
+    "A": A,
+    "B": B,
+    "n": n,
+    "D1": D1,
+    "D2": D2,
+    "D3": D3,
+    "uts": uts,
+    "kc": kc,
+    "mu": mu,
+    "kappa": kappa,
+}
+
+include_wear = True
 
 for id, massScale in enumerate(massScales):
 
-    fileName = "sim" + str(id)
-    ScratchModel, SubstratePart, SubstrateSet = ScratchModelSetup(
-        depth=depth,
+    fileName = "MassScale" + str(massScale)
+    ScratchModel, SubstratePart = ScratchModelSetup(
         SubstrateSizeY=meshSize[meshSizeIdx],
         SubstrateSizeX=meshSize[meshSizeIdx],
         SubstrateSizeZ=meshSize[meshSizeIdx],
-        target_time_increment=0.0,
         mass_scale=massScale,
+        use_ALE=True,
+        include_wear=include_wear,
     )
 
     material = SubstrateMaterialAssignment(
         ScratchModel,
         SubstratePart,
-        SubstrateSet,
         rho=rho,
         youngs_modulus=E,
         poisson_ratio=nu,
     )
-
     material.JohnsonCookHardening(A=A, B=B, n=n)
     material.JohnsonCookDamage(d1=D1, d2=D2, d3=D3)
-    material.DamageEvolution(kc=kc, E=E, nu=nu)
+    material.DamageEvolution(kc=kc, uts=uts, E=E, nu=nu)
+    # material.DamageEvolution(u_pl_f)
     material.SectionAssignment()
-    material.UpdateFriction(mu)
+    material.UpdateFrictionAndWear(mu, include_wear, kappa)
 
     #### ------------------------------ ####
     #           Create Job
     #### ------------------------------ ####
-    mdb.Job(
+    job = mdb.Job(
         activateLoadBalancing=False,
         atTime=None,
         contactPrint=OFF,
@@ -92,8 +109,78 @@ for id, massScale in enumerate(massScales):
         multiprocessingMode=MPI,
         name=jobName,
         nodalOutputPrecision=SINGLE,
-        numCpus=num_cpus,
-        numDomains=num_domains,
+        numCpus=C.num_cpus,
+        numDomains=C.num_domains,
+        parallelizationMethodExplicit=DOMAIN,
+        queue=None,
+        resultsFormat=ODB,
+        scratch="",
+        type=ANALYSIS,
+        userSubroutine="",
+        waitHours=0,
+        waitMinutes=0,
+    )
+
+    job.writeInput(consistencyChecking=OFF)
+
+    if include_wear:
+        # Go into .inp file and locate:
+        #
+        # *Surface Property, name=IntProp-2
+        # *WEAR SURFACE PROPERTIES, FRIC COEF DEPENDENT=YES, UNITLESS WEAR COEF=NO
+        # 0.0001, , , , **
+        #
+        # There is a bug here, it should be:
+        #
+        # 0.0001, , , ,
+        # **
+        #
+        # This is fixed here.
+        trigger_found = False
+        inpFile = jobName + ".inp"
+        with open(inpFile, "r") as file:
+            lines = file.readlines()
+        for i, line in enumerate(lines):
+            if "*WEAR SURFACE PROPERTIES" in line:
+                trigger_found = True
+                # next line is the one we want to change
+                if i + 1 < len(lines):
+                    old_line = lines[i + 1]
+
+                    new_line = f" {kappa}, , , , \n**\n"
+
+                    lines[i + 1] = new_line
+
+                break
+
+        if not trigger_found:
+            raise ValueError(
+                f"Could not find the trigger line in the inp file. Old line: {old_line}"
+            )
+
+        # Write the modified content back
+        # inpFileBackup = jobName + "Backup" + ".inp"
+        with open(inpFile, "w") as f:
+            f.writelines(lines)
+
+    job = mdb.JobFromInputFile(
+        name=jobName,
+        inputFileName=jobName + ".inp",
+        activateLoadBalancing=False,
+        atTime=None,
+        # contactPrint=OFF,
+        # description="",
+        # echoPrint=OFF,
+        explicitPrecision=SINGLE,
+        # historyPrint=OFF,
+        memory=90,
+        memoryUnits=PERCENTAGE,
+        # model="Model-1",
+        # modelPrint=OFF,
+        multiprocessingMode=MPI,
+        nodalOutputPrecision=SINGLE,
+        numCpus=C.num_cpus,
+        numDomains=C.num_domains,
         parallelizationMethodExplicit=DOMAIN,
         queue=None,
         resultsFormat=ODB,
@@ -107,9 +194,21 @@ for id, massScale in enumerate(massScales):
     #### ------------------------------ ####
     #             Submit Job
     #### ------------------------------ ####
-    mdb.jobs[jobName].submit(consistencyChecking=OFF)
-    mdb.jobs[jobName].waitForCompletion()
+    job.submit(consistencyChecking=OFF)
+    job.waitForCompletion()
 
-    PostProcess(jobName, fileName)
+    PostProcess(jobName, fileName, material_params)
 
     mdb.close()
+    sta_file = jobName + ".sta"
+    target_dir = "SimDataOutputs/"
+    new_name = fileName + ".sta"
+    dst_file = os.path.join(target_dir, new_name)
+    shutil.move(sta_file, dst_file)
+
+    odb_file = jobName + ".odb"
+    target_dir = "SimDataOutputs/"
+    new_name = fileName + ".odb"
+    dst_file = os.path.join(target_dir, new_name)
+    shutil.move(odb_file, dst_file)
+cleanupAbaqusJunk()
--- MeshConvergence.py
+++ MeshConvergence.py
@@ -8,6 +8,8 @@ import os
 # from material_parameters import parameters
 import numpy as np
 import shutil
+from cleanup import cleanupAbaqusJunk
+import ProgressiveLoadScratch.Constants as C
 
 
 ### ---------------- ###
@@ -18,13 +20,12 @@ jobName = "MeshConvergence"
 # Array of shape (#mesh combinations, 3). Rows are mesh combinations, coloums are [meshSizeX, meshSizeY, meshSizeZ],
 meshSizes = np.array(
     [
-        [0.040, 0.040, 0.040],
+        # [0.040, 0.040, 0.040],
         # [0.030, 0.030, 0.030],
         # [0.020, 0.020, 0.020],
-        # [0.010, 0.020, 0.020],
-        # [0.010, 0.020, 0.010],
+        # [0.015, 0.015, 0.015],
         # [0.010, 0.010, 0.010],
-        # [0.008, 0.008, 0.008],
+        [0.008, 0.008, 0.008],
         # [0.005, 0.005, 0.005],
     ]
 )
@@ -41,38 +42,57 @@ if not os.path.exists(rundir):
 os.chdir(rundir)
 
 
-depth = -25e-3
-rho = 7.8e-09
-E = 210000.0
+rho = 8.0e-09
+E = 200000.0
 nu = 0.3
-A = 1430.0
-B = 2545.0
-n = 0.7
-D1 = 0.028
-D2 = 1.0
-D3 = -0.916
-uts = 1600.0
+A = 700.0
+B = 700.0
+n = 0.5
+D1 = 1.0
+D2 = 0.5
+D3 = -0.25
+uts = 1000.0
 kc = 2000
-mu = 0.2
+mu = 0.1
+kappa = 1e-4
 
+# make to dict
+material_params = {
+    "rho": rho,
+    "E": E,
+    "nu": nu,
+    "A": A,
+    "B": B,
+    "n": n,
+    "D1": D1,
+    "D2": D2,
+    "D3": D3,
+    "uts": uts,
+    "kc": kc,
+    "mu": mu,
+    "kappa": kappa,
+}
+
+include_wear = True
 
 for id, meshSize in enumerate(meshSizes):
 
     fileName = (
         "mesh" + str(meshSize[0]) + "_" + str(meshSize[1]) + "_" + str(meshSize[2])
     )
-    ScratchModel, SubstratePart, SubstrateSet = ScratchModelSetup(
-        depth=depth,
+
+    ScratchModel, SubstratePart = ScratchModelSetup(
         SubstrateSizeY=meshSize[0],
         SubstrateSizeX=meshSize[1],
         SubstrateSizeZ=meshSize[2],
-        target_time_increment=0.0,
+        mass_scale=10e4,
+        use_ALE=False,
+        include_wear=include_wear,
     )
 
     material = SubstrateMaterialAssignment(
         ScratchModel,
         SubstratePart,
-        SubstrateSet,
         rho=rho,
         youngs_modulus=E,
         poisson_ratio=nu,
@@ -82,12 +102,12 @@ for id, meshSize in enumerate(meshSizes):
     material.JohnsonCookDamage(d1=D1, d2=D2, d3=D3)
     material.DamageEvolution(kc=kc, uts=uts, E=E, nu=nu)
     material.SectionAssignment()
-    material.UpdateFriction(mu)
+    material.UpdateFrictionAndWear(mu, kappa)
 
     #### ------------------------------ ####
     #           Create Job
     #### ------------------------------ ####
-    mdb.Job(
+    job = mdb.Job(
         activateLoadBalancing=False,
         atTime=None,
         contactPrint=OFF,
@@ -114,13 +134,82 @@ for id, meshSize in enumerate(meshSizes):
         waitMinutes=0,
     )
 
+    job.writeInput(consistencyChecking=OFF)
+
+    if include_wear:
+        # Go into .inp file and locate:
+        #
+        # *Surface Property, name=IntProp-2
+        # *WEAR SURFACE PROPERTIES, FRIC COEF DEPENDENT=YES, UNITLESS WEAR COEF=NO
+        # 0.0001, , , , **
+        #
+        # There is a bug here, it should be:
+        #
+        # 0.0001, , , ,
+        # **
+        #
+        # This is fixed here.
+        trigger_found = False
+        inpFile = jobName + ".inp"
+        with open(inpFile, "r") as file:
+            lines = file.readlines()
+        for i, line in enumerate(lines):
+            if "*WEAR SURFACE PROPERTIES" in line:
+                trigger_found = True
+                # next line is the one we want to change
+                if i + 1 < len(lines):
+                    old_line = lines[i + 1]
+
+                    new_line = f" {kappa}, , , , \n**\n"
+
+                    lines[i + 1] = new_line
+
+                break
+
+        if not trigger_found:
+            raise ValueError(
+                f"Could not find the trigger line in the inp file. Old line: {old_line}"
+            )
+
+        # Write the modified content back
+        # inpFileBackup = jobName + "Backup" + ".inp"
+        with open(inpFile, "w") as f:
+            f.writelines(lines)
+
+    job = mdb.JobFromInputFile(
+        name=jobName,
+        inputFileName=jobName + ".inp",
+        activateLoadBalancing=False,
+        atTime=None,
+        # contactPrint=OFF,
+        # description="",
+        # echoPrint=OFF,
+        explicitPrecision=SINGLE,
+        # historyPrint=OFF,
+        memory=90,
+        memoryUnits=PERCENTAGE,
+        # model="Model-1",
+        # modelPrint=OFF,
+        multiprocessingMode=MPI,
+        nodalOutputPrecision=SINGLE,
+        numCpus=C.num_cpus,
+        numDomains=C.num_domains,
+        parallelizationMethodExplicit=DOMAIN,
+        queue=None,
+        resultsFormat=ODB,
+        scratch="",
+        type=ANALYSIS,
+        userSubroutine="",
+        waitHours=0,
+        waitMinutes=0,
+    )
     #### ------------------------------ ####
     #             Submit Job
     #### ------------------------------ ####
-    mdb.jobs[jobName].submit(consistencyChecking=OFF)
-    mdb.jobs[jobName].waitForCompletion()
+    job.submit(consistencyChecking=OFF)
+    job.waitForCompletion()
 
-    PostProcess(jobName, fileName)
+    PostProcess(jobName, fileName, material_params)
 
     mdb.close()
 
@@ -135,3 +224,5 @@ for id, meshSize in enumerate(meshSizes):
     new_name = fileName + ".odb"
     dst_file = os.path.join(target_dir, new_name)
     shutil.move(odb_file, dst_file)
+
+cleanupAbaqusJunk()
